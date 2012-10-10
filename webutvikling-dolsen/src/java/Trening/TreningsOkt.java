9c51d9335984e9b952375072bf8f5daacda98313
Martin <Martin@158.38.121.143>
2012-10-10 16:14:20 +0200

    martinB

--- webutvikling-dolsen/src/java/Trening/TreningsOkt.java
+++ webutvikling-dolsen/src/java/Trening/TreningsOkt.java
@@ -4,12 +4,56 @@
  */
 package Trening;
 
+import java.io.Serializable;
+import java.util.Date;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named("trening")
+@SessionScoped
 /**
  *
  * @author deb
  */
-public class TreningsOkt {
+public class TreningsOkt implements Serializable {
+
+    int oktNr;
+    java.util.Date dato = new Date();
+    int varighet;
+    String kategori;
+
+    public TreningsOkt(){
+    }
     
+    public TreningsOkt(int oktNr, Date dato, int varighet, String kategori) {
+        this.oktNr = oktNr;
+        this.dato = dato;
+        this.varighet = varighet;
+        this.kategori = kategori;
+    }
     
-    
+    public int getOktNr(){
+        return oktNr;
+    }
+    public java.sql.Date getSqlDate(){
+        return new java.sql.Date(dato.getYear(), dato.getMonth(), dato.getDate());
+    }
+    public Date getDate(){
+        return dato;
+    }
+    public int getVarighet(){
+        return varighet;
+    }
+    public String getKategori(){
+        return kategori;
+    }
+    public void setOktNr(int nyOktNr){
+        oktNr = nyOktNr;
+    }
+    public void setDate(Date nyDato){
+        dato = nyDato;
+    }
+    public void setVarighet(int oktVarighet){
+        varighet = oktVarighet;
+    }
 }
