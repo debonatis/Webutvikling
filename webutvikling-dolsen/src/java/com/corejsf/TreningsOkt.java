7a8072b3c80de4ec66532233ee3d142cccbdd43d
Martin <Martin@158.38.121.143>
2012-10-10 16:24:34 +0200

    martinB

---
+++ webutvikling-dolsen/src/java/com/corejsf/TreningsOkt.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf;
+
+/**
+ *
+ * @author deb
+ */
+public class TreningsOkt {
+    
+    
+    
+}
