5f2af6377e17755ed88485731857c9f25697a90f
Martin <Martin@158.38.121.143>
2012-10-14 17:09:02 +0200

    martinB

---
+++ webutvikling-dolsen/src/java/com/corejsf/Oversikt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Martin
+ */
+public class Oversikt implements Serializable{
+    
+    private ArrayList<TreningsOkt> alleOkter = new ArrayList();
+    
+    public ArrayList<TreningsOkt> getAlleOkter(){
+        return null;
+    }
+    
+}
