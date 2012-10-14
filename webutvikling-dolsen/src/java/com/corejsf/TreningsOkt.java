886c0e1287b860211ab7e726583125a631055797
deb <deb@deb-BN.ka.hist.no>
2012-10-14 21:00:58 +0200

    Han helvett

--- webutvikling-dolsen/src/java/com/corejsf/TreningsOkt.java
+++ webutvikling-dolsen/src/java/com/corejsf/TreningsOkt.java
@@ -80,7 +80,7 @@ public final class TreningsOkt implements Serializable {
         return kategori;
     }
 
-    public static void setOktNr(int nyOktNr) {
+    public void setOktNr(int nyOktNr) {
         oktNr = nyOktNr;
     }
 
--- webutvikling-dolsen/src/java/com/corejsf/treningsOktBehandler.java
+++ webutvikling-dolsen/src/java/com/corejsf/treningsOktBehandler.java
@@ -59,7 +59,7 @@ public class treningsOktBehandler implements Serializable {
         if (!tempOkt.getKategori().trim().equals("")) {
 
             TreningsOkt nyOkt;
-            nyOkt = new TreningsOkt(TreningsOkt.getOktNr(), tempOkt.getDate(),
+            nyOkt = new TreningsOkt(tempOkt.getOktNr(), tempOkt.getDate(),
                     tempOkt.getVarighet(), tempOkt.getKategori(), tempOkt.getKategori());
 
             nyOversikt.registrerNyOkt(nyOkt);
