c364216743c68284281b2b7089c74d7636163a36
Jørgen <Jrgen@158.38.189.174>
2012-10-24 14:48:25 +0200

    local changer stuff

--- webutvikling-dolsen/src/java/com/corejsf/treningsOktBehandler.java
+++ webutvikling-dolsen/src/java/com/corejsf/treningsOktBehandler.java
@@ -5,9 +5,13 @@
 package com.corejsf;
 
 import java.io.Serializable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.ConcurrentModificationException;
+import java.util.Date;
 import java.util.List;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -23,14 +27,13 @@ import org.hibernate.validator.constraints.Range;
 public class treningsOktBehandler implements Serializable {
 
     public synchronized List<OktStatus> getTemptreningsOkter() {
-
         temptreningsOkter = Collections.synchronizedList(new ArrayList<OktStatus>());
-
         tempOkt.nullstill();
         temptreningsOkter.add(new OktStatus(tempOkt));
-
         return temptreningsOkter;
     }
+    
+    private ArrayList<TreningsOkt> objects;
     private Oversikt nyOversikt = new Oversikt();
     private List<OktStatus> treningsOkter = Collections.synchronizedList(new ArrayList<OktStatus>());
     private List<OktStatus> temptreningsOkter = Collections.synchronizedList(new ArrayList<OktStatus>());
@@ -41,7 +44,7 @@ public class treningsOktBehandler implements Serializable {
     int maned = 0;
     private int mick = 0;
     private boolean nyOkt = false;
-
+    
     public synchronized boolean isNyOkt() {
         return nyOkt;
     }
@@ -121,7 +124,7 @@ public class treningsOktBehandler implements Serializable {
                 mick++;
                 TreningsOkt nyOkt;
                 nyOkt = new TreningsOkt((tempOkt.getOktNr() + mick), tempOkt.getDate(),
-                        tempOkt.getVarighet(), tempOkt.getKategori(), tempOkt.getTekst());
+                        tempOkt.getVarighet(), tempOkt.getKategori(), tempOkt.getTekst(), tempOkt.getBrukernavn());
 
                 nyOversikt.registrerNyOkt(nyOkt);
                 treningsOkter.add(new OktStatus(nyOkt));
@@ -146,6 +149,33 @@ public class treningsOktBehandler implements Serializable {
             oppdater();
         }
         return "success";
+    }
+    
+    public void updateLists() {
+        TreningsOkt helpObject;
+        objects = new ArrayList<TreningsOkt>();
+        DBConnection conn = new DBConnection();
+        Statement st = null;
+        
+        ResultSet rs = null;
+        try{
+            st = conn.getConn().createStatement();
+            rs = st.executeQuery("SELECT * FROM WAPLJ.STUDIEOEKT");
+            // WHERE BRUKERNAVN = '" + user + "' (for senere bruk)
+
+            while (rs.next()) {
+                helpObject = new TreningsOkt(rs.getInt("OKTNR"), rs.getDate("DATO"), 
+                        rs.getInt("VARIGHET"), rs.getString("KATEGORINAVN"), 
+                        rs.getString("TEKST"), rs.getString("BRUKERNAVN"));
+                objects.add(helpObject);
+            }
+        } catch (SQLException e) {
+            conn.failed(); //Rollback
+        } finally {
+            conn.closeS(st);
+            conn.closeR(rs);
+            conn.close();
+        }
     }
 
     public synchronized int getManed() {
