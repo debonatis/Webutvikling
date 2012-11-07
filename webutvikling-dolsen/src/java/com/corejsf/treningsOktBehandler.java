b61986343f0022d653902e953263bd6acdac2562
deb <deb@deb-BN.ka.hist.no>
2012-11-07 15:29:07 +0100

    Han helvett

--- webutvikling-dolsen/src/java/com/corejsf/treningsOktBehandler.java
+++ webutvikling-dolsen/src/java/com/corejsf/treningsOktBehandler.java
@@ -19,7 +19,6 @@ import java.util.TimeZone;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.view.facelets.FaceletException;
 import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Range;
--- webutvikling-dolsen/src/java/com/corejsf/userBean.java
+++ webutvikling-dolsen/src/java/com/corejsf/userBean.java
@@ -1,13 +1,9 @@
 package com.corejsf;
 
-
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,16 +11,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 import javax.annotation.Resource;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.sql.DataSource;
-
 
 @Named("user")
 @SessionScoped
-public class userBean implements Serializable{
+public class userBean implements Serializable {
 
     private String name;
     private String password;
@@ -34,15 +33,18 @@ public class userBean implements Serializable{
     private static final Logger logger = Logger.getLogger("com.corejsf");
     @Resource(name = "jdbc/waplj-prosjekt")
     private DataSource ds;
+    private FacesMessage fm = new FacesMessage();
+    private FacesContext fc;
 
     public void changePassword() {
-        if(loggedIn); //do_changePassword(password);
+        if (loggedIn); //do_changePassword(password);
     }
 
     public void setNewPassword(String newPassword) {
         this.newPassword = newPassword;
     }
-    public String getNewPassword(){
+
+    public String getNewPassword() {
         return newPassword;
     }
 
@@ -50,96 +52,91 @@ public class userBean implements Serializable{
         return password;
     }
 
-    public void setPassword(String newValue){
+    public void setPassword(String newValue) {
         password = newValue;
     }
 
 
     /* quote fra boka: "If your container does not
      support resource injection, add this constructor:
-    public UserBean() {
-        try {
-            Context ctx = new InitialContext();
-            ds = (DataSource) ctx.lookup("java:comp/env/jdbc/mydb");
-        } catch (NamingException ex) {
-            logger.log(level.SEVERE, "DataSource lookup failed", ex);
-        }
-    }*/
-
-
-    public String getName(){
+     public UserBean() {
+     try {
+     Context ctx = new InitialContext();
+     ds = (DataSource) ctx.lookup("java:comp/env/jdbc/mydb");
+     } catch (NamingException ex) {
+     logger.log(level.SEVERE, "DataSource lookup failed", ex);
+     }
+     }*/
+    public String getName() {
         return name;
     }
-    public void setName(String newValue){
+
+    public void setName(String newValue) {
         password = newValue;
     }
     //Count = antall ganger besøkt
-   // public int getCount(){
-     //   return count;
+    // public int getCount(){
+    //   return count;
     //}
-    public String login(){
-        try{
+
+    public String login() {
+        try {
             doLogin();
-        }
-        catch(SQLException ex){
+        } catch (SQLException ex) {
             logger.log(Level.SEVERE, "login failed", ex);
             return "internalError";
 
         }
-        if(loggedIn)
+        if (loggedIn) {
             return "loginSuccess";
-        else return "loginFailure";
+        } else {
+            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Username or password is wrong!", "ja,Oppdatering utført!");
+            fc = FacesContext.getCurrentInstance();
+            fc.addMessage("null", fm);
+            fc.renderResponse();
+            return "loginFailure";
+
+        }
     }
 
-    public String logout(){
+    public String logout() {
         loggedIn = false;
         return "login";
     }
 
-    public void doLogin() throws SQLException{
-        if(ds==null){
+    public void doLogin() throws SQLException {
+        if (ds == null) {
             throw new SQLException("No data source");
         }
-        Connection conn = ds.getConnection("waplj","waplj");
-        if(conn == null){
+        Connection conn = ds.getConnection("waplj", "waplj");
+        if (conn == null) {
             throw new SQLException("No conection");
         }
 
-        try{
+        try {
             conn.setAutoCommit(false);
             boolean committed = false;
-            try{
-                PreparedStatement passwordQuery = conn.prepareStatement("select PASSORD from waplj.BRUKER where brukernavn = '" + name +"'");
-                //passwordQuery.setString(1,name);
+            try {
+                PreparedStatement passwordQuery = conn.prepareStatement("select PASSORD from WAPLJ.BRUKER where brukernavn = '" + name + "'");
 
-                ResultSet result = passwordQuery.executeQuery();
 
-                if(!result.next()) return;
-                String storedPassword = result.getString("passord");
+                ResultSet k = passwordQuery.executeQuery();
+
+//                if (!k.next()) {
+//                    return;
+//                }
+                String storedPassword = k.getString("passord");
                 loggedIn = password.equals(storedPassword.trim());
-               // count = result.getInt("logincount");
-                
-                // viss vi ska mekke counter
-                //PreparedStatement updateCounterStat = conn.prepareStatement("UPDATE waplj.BRUKER SET logincount = logincount +1" + "WHERE USERNAME = ?");
-                //updateCounterStat.setString(1, name);
-                //updateCounterStat.executeUpdate();
 
                 conn.commit();
                 committed = true;
-            }finally{
-                if(!committed) conn.rollback();
+            } finally {
+                if (!committed) {
+                    conn.rollback();
+                }
             }
-        }
-        finally{
+        } finally {
             conn.close();
         }
     }
-
-
-        
-    }
-
-    
-
-
- 
+}
