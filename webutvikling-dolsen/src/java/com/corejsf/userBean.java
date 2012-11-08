c0229de2758c39735ee3ba67113a1e39021c519f
Jørgen <Jrgen@158.38.189.174>
2012-11-08 16:09:08 +0100

    local changer stuff

--- webutvikling-dolsen/src/java/com/corejsf/userBean.java
+++ webutvikling-dolsen/src/java/com/corejsf/userBean.java
@@ -11,38 +11,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
-
 import javax.annotation.Resource;
-import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
+import javax.ws.rs.core.Request;
 
 @Named("user")
 @SessionScoped
 public class userBean implements Serializable {
 
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    private String rolle;
     private String password;
     private String newPassword;
     private int count;
     private boolean loggedIn;
     private static final Logger logger = Logger.getLogger("com.corejsf");
-   
-    private  @Resource(name = "jdbc/waplj-prosjekt") DataSource ds;
+    private @Resource(name = "jdbc/waplj-prosjekt")
+    DataSource ds;
     private FacesMessage fm = new FacesMessage();
     private FacesContext fc;
+
+    public String getRolle() {
+        return rolle == null ? "" : rolle;
+    }
+
+    public void setRolle(String rolle) {
+        this.rolle = rolle;
+    }
+
+    public String getName() {
+        if (name == null) {
+            getUserData();
+        }
+        return name == null ? "" : name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public void changePassword() {
         if (loggedIn); //do_changePassword(password);
@@ -75,69 +87,100 @@ public class userBean implements Serializable {
      logger.log(level.SEVERE, "DataSource lookup failed", ex);
      }
      }*/
-   
     //Count = antall ganger besøkt
     // public int getCount(){
     //   return count;
     //}
+//    public String login() {
+//        try {
+//            doLogin();
+//        } catch (SQLException ex) {
+//            logger.log(Level.SEVERE, "login failed", ex);
+//            return "internalError";
+//
+//        }
+//        if (loggedIn) {
+//            return "loginSuccess";
+//        } else {
+//            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Username or password is wrong!", "ja,Oppdatering utført!");
+//            fc = FacesContext.getCurrentInstance();
+//            fc.addMessage("null", fm);
+//            fc.renderResponse();
+//            return "loginFailure";
+//
+//        }
+//    }
+//
+//    public String logout() {
+//        loggedIn = false;
+//        return "login";
+//    }
+//
+//    public void doLogin() throws SQLException {
+//        if (ds == null) {
+//            throw new SQLException("No data source");
+//        }
+//        Connection conn = ds.getConnection("waplj", "waplj");
+//        if (conn == null) {
+//            throw new SQLException("No conection");
+//        }
+//
+//        try {
+//            conn.setAutoCommit(false);
+//            boolean committed = false;
+//            try {
+//                PreparedStatement passwordQuery = conn.prepareStatement("select BRUKER.PASSORD from WAPLJ.BRUKER where BRUKER.BRUKERNAVN = ? ");
+//                passwordQuery.setString(1, this.getName());
+//                ResultSet k = passwordQuery.executeQuery();
+//                conn.commit();
+//                if (!k.next()) {
+//                    return;
+//                }
+//                String storedPassword = k.getString(1);
+//                loggedIn = password.equals(storedPassword.trim());
+//
+//
+//                committed = true;
+//            } finally {
+//                if (!committed) {
+//                    conn.rollback();
+//                }
+//            }
+//        } finally {
+//            conn.close();
+//        }
+//    }
 
-    public String login() {
-        try {
-            doLogin();
-        } catch (SQLException ex) {
-            logger.log(Level.SEVERE, "login failed", ex);
-            return "internalError";
-
+    public void getUserData() {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        Object forsporrselobject = context.getRequest();
+        if (!(forsporrselobject instanceof HttpServletRequest)) {
+            logger.log(Level.SEVERE, "Det forespurte objektet er av type {0}", forsporrselobject.getClass());
+            return;
         }
-        if (loggedIn) {
-            return "loginSuccess";
-        } else {
-            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Username or password is wrong!", "ja,Oppdatering utført!");
-            fc = FacesContext.getCurrentInstance();
-            fc.addMessage("null", fm);
-            fc.renderResponse();
-            return "loginFailure";
-
-        }
+        HttpServletRequest foresporrsel = (HttpServletRequest) forsporrselobject;
+        name = foresporrsel.getRemoteUser();
     }
 
-    public String logout() {
-        loggedIn = false;
-        return "login";
+    public boolean isInRole() {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        Object forsporrselobject = context.getRequest();
+        if (!(forsporrselobject instanceof HttpServletRequest)) {
+            logger.log(Level.SEVERE, "Det forespurte objektet er av type {0}", forsporrselobject.getClass());
+            return false;
+        }
+        HttpServletRequest foresporrsel = (HttpServletRequest) forsporrselobject;
+        return foresporrsel.isUserInRole(rolle);
     }
-
-    public void doLogin() throws SQLException {
-        if (ds == null) {
-            throw new SQLException("No data source");
+    public boolean isAdmin(){
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        Object forsporrselobject = context.getRequest();
+        if (!(forsporrselobject instanceof HttpServletRequest)) {
+            logger.log(Level.SEVERE, "Det forespurte objektet er av type {0}", forsporrselobject.getClass());
+            return false;
         }
-        Connection conn = ds.getConnection("waplj", "waplj");
-        if (conn == null) {
-            throw new SQLException("No conection");
-        }
-
-        try {
-            conn.setAutoCommit(false);
-            boolean committed = false;
-            try {
-                PreparedStatement passwordQuery = conn.prepareStatement("select BRUKER.PASSORD from WAPLJ.BRUKER where BRUKER.BRUKERNAVN = ? ");
-                passwordQuery.setString(1, this.getName()); 
-                ResultSet k = passwordQuery.executeQuery();
-                conn.commit();               
-                if (!k.next()) {
-                    return;
-                }
-                String storedPassword = k.getString(1);
-                loggedIn = password.equals(storedPassword.trim());
-
-
-                committed = true;
-            } finally {
-                if (!committed) {
-                    conn.rollback();
-                }
-            }
-        } finally {
-            conn.close();
-        }
+        String hjelp = "admin";
+        HttpServletRequest foresporrsel2 = (HttpServletRequest) forsporrselobject;
+        return foresporrsel2.isUserInRole(hjelp);
     }
 }
