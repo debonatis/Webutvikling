b93bddae56eba36221ed2dc03b60362707b05419
deb <deb@deb-BN.ka.hist.no>
2012-11-19 19:51:43 +0100

    Han helvett

--- webutvikling-dolsen/src/java/com/corejsf/validator/ValidatorTekst3.java
+++ webutvikling-dolsen/src/java/com/corejsf/validator/ValidatorTekst3.java
@@ -7,6 +7,8 @@ package com.corejsf.validator;
 import com.corejsf.DBadm.DBController;
 import com.corejsf.brukerAdm.BrukerStatus;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
@@ -21,24 +23,31 @@ import javax.faces.validator.ValidatorException;
 @FacesValidator("validatorTekst3")
 public class ValidatorTekst3 extends DBController implements Validator {
 
-   
-    boolean brukernavnOK = true;
+    private boolean brukerNavnOK = true;
+    private String brukerNavnKrav = "(.{6,10})";
+    private Pattern brukerNavnSjekk;
+    private Matcher sjekker;
 
     @Override
     public void validate(FacesContext facesContext, UIComponent uIComponent, Object object) throws ValidatorException {
-        String innlagtTekst = (String) object;      
+        String innlagtTekst = (String) object;
+        brukerNavnSjekk = Pattern.compile(brukerNavnKrav);
+        sjekker = brukerNavnSjekk.matcher(innlagtTekst);
+        brukerNavnOK = sjekker.matches();
         
-        List<BrukerStatus> brukere = getAlleBrukere();
         
-        for(BrukerStatus k : brukere){
-            if(k.getBruker().getName().trim().equalsIgnoreCase(innlagtTekst)){
-                brukernavnOK = false;
-            }
-        }       
 
-        if (!brukernavnOK) {
+        List<BrukerStatus> brukere = getAlleBrukere();
+
+        for (BrukerStatus k : brukere) {
+            if (k.getBruker().getName().trim().equalsIgnoreCase(innlagtTekst)) {
+                brukerNavnOK = false;
+            }
+        }
+
+        if (!brukerNavnOK) {
             FacesMessage message = new FacesMessage();
-            message.setSummary("This username is already in use, or it is one that is too similar. Write a new one!");
+            message.setSummary("This username is already in use, or it is one that is too similar. Write a new one! The username must have a lenght between 6 to 10 characters!");
 
             throw new ValidatorException(message);
 
