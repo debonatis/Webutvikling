afeba70125b1b760e679d8d602169fce2c587d25
deb <deb@deb-BN.ka.hist.no>
2012-11-19 13:08:13 +0100

    Han helvett

---
+++ webutvikling-dolsen/src/java/com/corejsf/DBadm/DBController.java
@@ -0,0 +1,222 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.corejsf.DBadm;
+
+import com.corejsf.OktStatus;
+import com.corejsf.TreningsOkt;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.ConcurrentModificationException;
+import java.util.Date;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author deb
+ */
+public class DBController {
+
+    private static FacesMessage fm;
+    private static FacesContext fc;
+    private static List<OktStatus> dBtreningsobjekter = Collections.synchronizedList(new ArrayList<OktStatus>());
+    private static List<OktStatus> hjelp = Collections.synchronizedList(new ArrayList<OktStatus>());
+
+    public static synchronized List<OktStatus> getAlleTreningsOkter(String navn) {
+        TreningsOkt hjelpeobjekt;
+        dBtreningsobjekter.clear();
+        DBConnection conn = new DBConnection();
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.getConn().createStatement();
+            rs = st.executeQuery("SELECT * FROM WAPLJ.TRENING "
+                    + "where BRUKERNAVN = '" + navn + "'");
+
+
+            while (rs.next()) {
+                hjelpeobjekt = new TreningsOkt(rs.getInt("OKTNR"), new Date(rs.getDate("DATO").getTime()),
+                        rs.getInt("VARIGHET"), rs.getString("KATEGORINAVN"),
+                        rs.getString("TEKST"));
+                dBtreningsobjekter.add(new OktStatus(hjelpeobjekt));
+
+
+            }
+            conn.getConn().commit();
+            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alle Okter skaffet!", "ja,Okter skaffet!");
+            fc = FacesContext.getCurrentInstance();
+            fc.addMessage("null", fm);
+            fc.renderResponse();
+        } catch (SQLException e) {
+            conn.failed(); //Rollback
+        } finally {
+            conn.closeS(st);
+            conn.closeR(rs);
+            conn.close();
+
+
+            return dBtreningsobjekter;
+        }
+    }
+
+    public static synchronized void registrerTreningsOkt(TreningsOkt okt, String navn) {
+        //oktnr blir autogenerert i databasen
+        DBConnection conn = new DBConnection();
+        PreparedStatement reg = null;
+        String regTekst = "INSERT INTO WAPLJ.TRENING(dato, varighet, kategorinavn, tekst, brukernavn)"
+                + " VALUES (?,?,?,?,?)";
+        try {
+
+            conn.getConn().setAutoCommit(false);
+            reg = conn.getConn().prepareStatement(regTekst);
+            reg.setDate(1, okt.getSqlDate());
+            reg.setInt(2, okt.getVarighet());
+            reg.setString(3, okt.getKategori());
+            reg.setString(4, okt.getTekst());
+            reg.setString(5, navn);
+            reg.executeUpdate();
+            conn.getConn().commit();
+
+
+            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Nyregistrering fullført!", "ja,Nyregistreing fullført!");
+            fc = FacesContext.getCurrentInstance();
+            fc.addMessage("null", fm);
+            fc.renderResponse();
+
+
+
+        } catch (SQLException e) {
+            conn.failed();
+        } finally {
+            conn.closeP(reg);
+            conn.close();
+        }
+
+
+    }
+
+    public static synchronized void slettTreningsOkt(TreningsOkt objekt, int i, String navn) {
+        DBConnection conn = new DBConnection();
+        Statement st = null;
+        try {
+            st = conn.getConn().createStatement();
+            st.executeUpdate("DELETE FROM WAPLJ.TRENING WHERE OKTNR =" + objekt.getOktNr() + " AND BRUKERNAVN = '" + navn + "'");
+            st.getConnection().commit();
+            if (i == 1) {
+                fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sletting utført!", "ja,Sletting utført!");
+                fc = FacesContext.getCurrentInstance();
+                fc.addMessage("null", fm);
+                fc.renderResponse();
+            }
+
+
+
+
+
+        } catch (SQLException e) {
+            conn.failed();
+
+
+        } finally {
+            conn.closeS(st);
+            conn.close();
+        }
+
+    }
+
+    public static synchronized void oppdaterTreningsOktDB(List<OktStatus> treningsOkter, String navn) {
+
+        hjelp.clear();
+        if (!(treningsOkter.isEmpty())) {
+            for (OktStatus j : treningsOkter) {
+                if (j.getTreningsikOkt().isEndret()) {
+                    j.getTreningsikOkt().setEndret(false);
+                    hjelp.add(j);
+                }
+            }
+        }
+
+
+        DBConnection conn = new DBConnection();
+        PreparedStatement oppdaterOkter = null;
+        String oppdaterString =
+                "update WAPLJ.TRENING "
+                + "set DATO = ?, VARIGHET= ?, "
+                + "KATEGORINAVN= ?, TEKST= ? "
+                + "where OKTNR = ? AND BRUKERNAVN= ?";
+        if (!hjelp.isEmpty()) {
+
+            try {
+
+                conn.getConn().setAutoCommit(false);
+                oppdaterOkter = conn.getConn().prepareStatement(oppdaterString);
+                for (OktStatus f : hjelp) {
+                    oppdaterOkter.setDate(1, f.getTreningsikOkt().getSqlDate());
+                    oppdaterOkter.setInt(2, f.getTreningsikOkt().getVarighet());
+                    oppdaterOkter.setString(3, f.getTreningsikOkt().getKategori());
+                    oppdaterOkter.setString(4, f.getTreningsikOkt().getTekst());
+                    oppdaterOkter.setInt(5, f.getTreningsikOkt().getOktNr());
+                    oppdaterOkter.setString(6, navn);
+                    oppdaterOkter.executeUpdate();
+                    conn.getConn().commit();
+
+                }
+                fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Oppdatering utført!", "ja,Oppdatering utført!");
+                fc = FacesContext.getCurrentInstance();
+                fc.addMessage("null", fm);
+                fc.renderResponse();
+
+
+            } catch (SQLException e) {
+                conn.failed();
+                if (conn.getConn() != null) {
+                    try {
+                        System.err.print("Endring har blitt trekk tilbake");
+                        conn.getConn().rollback();
+                    } catch (SQLException excep) {
+                        conn.failed();
+                    }
+                }
+
+
+            } finally {
+                conn.closeS(oppdaterOkter);
+                conn.close();
+
+            }
+
+        }
+
+
+    }
+
+    public static synchronized void slettAlleOkter() {
+
+        DBConnection conn = new DBConnection();
+        Statement st = null;
+
+        try {
+            st = conn.getConn().createStatement();
+            st.execute("TRUNCATE TABLE WAPLJ.TRENING");
+
+            conn.getConn().commit();
+            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alle Okter slettet!", "ja,Okter skaffet!");
+            fc = FacesContext.getCurrentInstance();
+            fc.addMessage("null", fm);
+            fc.renderResponse();
+        } catch (SQLException e) {
+            conn.failed();
+        } finally {
+            conn.closeS(st);
+            conn.close();
+        }
+
+    }
+}
--- webutvikling-dolsen/src/java/com/corejsf/treningsOktBehandler.java
+++ webutvikling-dolsen/src/java/com/corejsf/treningsOktBehandler.java
@@ -5,6 +5,7 @@
 package com.corejsf;
 
 import com.corejsf.DBadm.DBConnection;
+import com.corejsf.DBadm.DBController;
 import java.io.Serializable;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -146,21 +147,7 @@ public class treningsOktBehandler implements Serializable {
 
     @RolesAllowed("admin")
     public synchronized void slettAlleOkter() {
-        try {
-            for (OktStatus z : treningsOkter) {
-                TreningsOkt k = z.getTreningsikOkt();
-                nyOversikt.slettOkt(k);
-                slettTreningsOkt(k, 0);
-                treningsOkter.remove(z);
-            }
-            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sletting av all data utført!", "ja,Sletting utført!");
-            fc = FacesContext.getCurrentInstance();
-            fc.addMessage("null", fm);
-            fc.renderResponse();
-
-        } catch (ConcurrentModificationException s) {
-            slettAlleOkter();
-        }
+        DBController.slettAlleOkter();
 
     }
 
@@ -177,82 +164,27 @@ public class treningsOktBehandler implements Serializable {
 
                 nyOversikt.registrerNyOkt(nyOkt);
                 treningsOkter.add(new OktStatus(nyOkt));
-                registrerTreningsOkt(nyOkt);
+                DBController.registrerTreningsOkt(nyOkt, getNavn());
                 tempOkt = new TreningsOkt();
             }
 
 
 
             if (!(treningsOkter.isEmpty())) {
-//                for (OktStatus r : treningsOkter) {
-//                    if (r.getSkalSlettes()) {
-//                        for (TreningsOkt e : nyOversikt.getAlleOkter()) {
-//                            if (e.equals(r.getTreningsikOkt())) {
-//                                slettTreningsOkt(e, 1);
-//                                nyOversikt.slettOkt(e);
-//
-//                            }
-//                        }
-//                        treningsOkter.remove(r);
-//                    }
-//                }
                 int indeks = treningsOkter.size() - 1;
                 while (indeks >= 0) {
                     OktStatus ts = treningsOkter.get(indeks);
-                    if (ts.getSkalSlettes()) {  
+                    if (ts.getSkalSlettes()) {
                         nyOversikt.slettOkt(ts.getTreningsikOkt());
-                        treningsOkter.remove(indeks);  
-                        slettTreningsOkt(ts.getTreningsikOkt(), 1);
+                        treningsOkter.remove(indeks);
+                        DBController.slettTreningsOkt(ts.getTreningsikOkt(), 1, getNavn());
                     }
                     indeks--;
                 }
             }
 
-            oppdaterTreningsOktDB();
-            getAlleTreningsOkter();
-
-
-
-
-
-        } catch (ConcurrentModificationException e) {
-            oppdater();
-        }
-        return "success";
-    }
-
-    public synchronized void getAlleTreningsOkter() {
-        TreningsOkt hjelpeobjekt;
-        DBtreningsobjekter.clear();
-        DBConnection conn = new DBConnection();
-        Statement st = null;
-        ResultSet rs = null;
-        try {
-            st = conn.getConn().createStatement();
-            rs = st.executeQuery("SELECT * FROM WAPLJ.TRENING "
-                    + "where BRUKERNAVN = '" + getNavn() + "'");
-
-
-            while (rs.next()) {
-                hjelpeobjekt = new TreningsOkt(rs.getInt("OKTNR"), new Date(rs.getDate("DATO").getTime()),
-                        rs.getInt("VARIGHET"), rs.getString("KATEGORINAVN"),
-                        rs.getString("TEKST"));
-                DBtreningsobjekter.add(new OktStatus(hjelpeobjekt));
-
-
-            }
-            conn.getConn().commit();
-            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Alle Okter skaffet!", "ja,Okter skaffet!");
-            fc = FacesContext.getCurrentInstance();
-            fc.addMessage("null", fm);
-            fc.renderResponse();
-        } catch (SQLException e) {
-            conn.failed(); //Rollback
-        } finally {
-            conn.closeS(st);
-            conn.closeR(rs);
-            conn.close();
-
+            DBController.oppdaterTreningsOktDB(treningsOkter, getNavn());
+            DBtreningsobjekter = DBController.getAlleTreningsOkter(getNavn());
             if (!DBtreningsobjekter.isEmpty()) {
                 nyOversikt.slettAlle();
                 treningsOkter.clear();
@@ -261,7 +193,10 @@ public class treningsOktBehandler implements Serializable {
                     treningsOkter.add(s);
                 }
             }
+        } catch (ConcurrentModificationException e) {
+            oppdater();
         }
+        return "success";
     }
 
     public int getManed() {
@@ -271,138 +206,6 @@ public class treningsOktBehandler implements Serializable {
     public void setManed(int Maned) {
 
         this.maned = Maned;
-
-    }
-
-    public synchronized boolean registrerTreningsOkt(TreningsOkt okt) {
-        //oktnr blir autogenerert i databasen
-        DBConnection conn = new DBConnection();
-        PreparedStatement reg = null;
-        String regTekst = "INSERT INTO WAPLJ.TRENING(dato, varighet, kategorinavn, tekst, brukernavn)"
-                + " VALUES (?,?,?,?,?)";
-        try {
-
-            conn.getConn().setAutoCommit(false);
-            reg = conn.getConn().prepareStatement(regTekst);
-            reg.setDate(1, okt.getSqlDate());
-            reg.setInt(2, okt.getVarighet());
-            reg.setString(3, okt.getKategori());
-            reg.setString(4, okt.getTekst());
-            reg.setString(5, getNavn());
-            reg.executeUpdate();
-            conn.getConn().commit();
-
-
-            fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Nyregistrering fullført!", "ja,Nyregistreing fullført!");
-            fc = FacesContext.getCurrentInstance();
-            fc.addMessage("null", fm);
-            fc.renderResponse();
-
-            return true;
-
-        } catch (SQLException e) {
-            conn.failed();
-            return false;
-        } finally {
-            conn.closeP(reg);
-            conn.close();
-        }
-
-
-    }
-
-    public synchronized boolean slettTreningsOkt(TreningsOkt objekt, int i) {
-        DBConnection conn = new DBConnection();
-        Statement st = null;
-        try {
-            st = conn.getConn().createStatement();
-            st.executeUpdate("DELETE FROM WAPLJ.TRENING WHERE OKTNR =" + objekt.getOktNr() + " AND BRUKERNAVN = '" + getNavn() + "'");
-            st.getConnection().commit();
-            if (i == 1) {
-                fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Sletting utført!", "ja,Sletting utført!");
-                fc = FacesContext.getCurrentInstance();
-                fc.addMessage("null", fm);
-                fc.renderResponse();
-            }
-
-            return true;
-
-
-
-        } catch (SQLException e) {
-            conn.failed();
-            return false;
-
-        } finally {
-            conn.closeS(st);
-            conn.close();
-        }
-
-    }
-
-    public synchronized boolean oppdaterTreningsOktDB() {
-
-        hjelp.clear();
-        if (!(treningsOkter.isEmpty())) {
-            for (OktStatus j : treningsOkter) {
-                if (j.getTreningsikOkt().isEndret()) {
-                    j.getTreningsikOkt().setEndret(false);
-                    hjelp.add(j);
-                }
-            }
-        }
-
-
-        DBConnection conn = new DBConnection();
-        PreparedStatement oppdaterOkter = null;
-        String oppdaterString =
-                "update WAPLJ.TRENING "
-                + "set DATO = ?, VARIGHET= ?, "
-                + "KATEGORINAVN= ?, TEKST= ? "
-                + "where OKTNR = ? AND BRUKERNAVN= ?";
-        if (!hjelp.isEmpty()) {
-
-            try {
-
-                conn.getConn().setAutoCommit(false);
-                oppdaterOkter = conn.getConn().prepareStatement(oppdaterString);
-                for (OktStatus f : hjelp) {
-                    oppdaterOkter.setDate(1, f.getTreningsikOkt().getSqlDate());
-                    oppdaterOkter.setInt(2, f.getTreningsikOkt().getVarighet());
-                    oppdaterOkter.setString(3, f.getTreningsikOkt().getKategori());
-                    oppdaterOkter.setString(4, f.getTreningsikOkt().getTekst());
-                    oppdaterOkter.setInt(5, f.getTreningsikOkt().getOktNr());
-                    oppdaterOkter.setString(6, getNavn());
-                    oppdaterOkter.executeUpdate();
-                    conn.getConn().commit();
-
-                }
-                fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "Oppdatering utført!", "ja,Oppdatering utført!");
-                fc = FacesContext.getCurrentInstance();
-                fc.addMessage("null", fm);
-                fc.renderResponse();
-
-
-            } catch (SQLException e) {
-                conn.failed();
-                if (conn.getConn() != null) {
-                    try {
-                        System.err.print("Endring har blitt trekk tilbake");
-                        conn.getConn().rollback();
-                    } catch (SQLException excep) {
-                        conn.failed();
-                    }
-                }
-                return false;
-
-            } finally {
-                conn.closeS(oppdaterOkter);
-                conn.close();                
-                return true;
-            }
-
-        }
-        return false;
 
     }
 }
